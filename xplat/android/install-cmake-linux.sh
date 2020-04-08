a401cdd19485a23210d9fc60ba5665fa8787af0e
Lincoln Bergeson <lincoln.bergeson@gmail.com>
2020-04-08 12:14:50 -0600

    updating travis

--- xplat/android/install-cmake-linux.sh
+++ xplat/android/install-cmake-linux.sh
@@ -2,6 +2,8 @@
 
 CMAKE_NAME="cmake-3.17.0-Linux-x86_64"
 SOURCE_URL="https://github.com/Kitware/CMake/releases/download/v3.17.0/$CMAKE_NAME.tar.gz"
+CMAKE_PATH="$(which cmake)"
 wget $SOURCE_URL
-tar xvf $CMAKE_NAME.tar.gz
-export PATH="$(pwd)/$CMAKE_NAME/bin:$PATH"
+tar xf $CMAKE_NAME.tar.gz
+sudo rm "$CMAKE_PATH"
+sudo ln -s "$(pwd)/$CMAKE_NAME/bin/cmake" "$CMAKE_PATH"
