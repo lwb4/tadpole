0dac85a4cf8012f9cb32e5a450b47f663c112569
JanPaul Bergeson <janpaulb@qualtrics.com>
2020-04-28 11:10:42 -0600

    Fix stdout/stderr redirects

--- xplat/web/repackage.sh
+++ xplat/web/repackage.sh
@@ -4,4 +4,4 @@ set -e
 echo "Repackaging due to changes ..."
 python ../../deps/emsdk/upstream/emscripten/tools/file_packager.py tadpole.data \
     --preload ../../assets/fonts ../../assets/images ../../assets/scripts ../../assets/sounds \
-    >/dev/null 2>&1
+    2>&1 >/dev/null
